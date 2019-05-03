fafb52013ea345d2ba84f3d8091726459e888569
lucas234 <ly_liubo@163.com>
2019-05-03 10:47:47 +0800

    Update identity_card.py

--- identity_card.py
+++ identity_card.py
@@ -34,7 +34,7 @@ class IdentityCard(object):
     def get_id_card_verify_number(cls, id_card):
         factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
         check_code_list = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]
-        check_sum = sum([a * b for a, b in zip(factor, [int(a.replace("-", "0").replace(".", "0")) for a in id_card[0:-1]])])
+        check_sum = sum([a * b for a, b in zip(factor, [int(a) for a in id_card[0:-1]])])
         return str(check_code_list[check_sum % 11])
 
     @classmethod
